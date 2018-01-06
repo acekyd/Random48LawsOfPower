cb037deb8ef4b4da0ba76673f7f96e877377e5fd
vakinwande <viktour19@gmail.com>
2018-01-06 10:31:27 +0200

    upgrades - background image, and more laws

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-	console.log(data);
-	var random = Math.floor(Math.random() * 48);
-	 document.getElementById('id').innerHTML = data[random].name;
-	 document.getElementById('law-title').innerHTML = data[random].title;
-	 document.getElementById('law-desc').innerHTML = data[random].desc;
+strimg ="#" 
+try
+{
+
+
+	url = "https://api.unsplash.com/photos/random/?client_id=f8c37e9ef128f09611217b491dcb047b5237346a86df3b89c347f1a6722f03da"
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", url, false);
+	xmlHttp.send(null);
+
+	resp = xmlHttp.responseText;
+
+	strimg = JSON.parse(resp)["urls"]["regular"]
+
+	var body = document.getElementsByTagName('body')[0];
+	body.style.backgroundImage = 'url(' + strimg + ')';
+
+}
+catch(e)
+{
+	console.log("image load failure")
+}
+
+var random = Math.floor(Math.random() * 78);
+
+document.getElementById('id').innerHTML = data[random].name;
+document.getElementById('law-title').innerHTML = data[random].title;
+document.getElementById('law-desc').innerHTML = data[random].desc;
+
